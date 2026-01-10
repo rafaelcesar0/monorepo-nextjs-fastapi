48a1a0b1574a3e5050371a9d70e09c9a413ed735
rafaelcesar0 <rafael.cesar.dev@gmail.com>
2026-01-10 12:10:54 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@node-python/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
